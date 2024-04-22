e78c0e53880a319db483decd85aa425eeb890890
Jaime Andrade <jaimeandrade454@gmail.com>
2024-04-22 18:06:06 -0500

    Modify the menus and use the controller to scroll

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:porfolio/providers/providers.dart';
 import 'package:porfolio/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/utils.dart';
 
@@ -10,6 +12,21 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+    void goto(GlobalKey key) {
+      final controller = uiProvider.scrollControllerMain;
+
+      final RenderBox renderBox =
+          key.currentContext!.findRenderObject() as RenderBox;
+      final position = renderBox.localToGlobal(Offset.zero);
+
+      controller.animateTo(
+        controller.offset + position.dy,
+        curve: Curves.easeInOut,
+        duration: Duration(milliseconds: 500),
+      );
+    }
+
     return Drawer(
       width: Constants.DRAWER_WIDTH,
       child: Column(
@@ -52,49 +69,62 @@ class CustomDrawer extends StatelessWidget {
                 ))
             ],
           )),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text(
-              'HOME',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          ButtonMenuItem(
+            title: 'HOME',
+            icon: Icons.home,
+            onTap: () => goto(uiProvider.profileKey),
           ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text(
-              'ABOUT',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          ButtonMenuItem(
+            title: 'ABOUT',
+            icon: Icons.menu_book_rounded,
+            onTap: () => goto(uiProvider.aboutKey),
           ),
-          ListTile(
-            leading: Icon(Icons.category),
-            title: Text(
-              'SERVICES',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          ButtonMenuItem(
+            title: 'SERVICES',
+            icon: Icons.category,
+            onTap: () => goto(uiProvider.servicesKey),
           ),
-          ListTile(
-            leading: Icon(Icons.school),
-            title: Text(
-              'EXPERIENCE',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          ButtonMenuItem(
+            title: 'EXPERIENCE',
+            icon: Icons.school,
+            onTap: () => goto(uiProvider.experienceKey),
           ),
-          ListTile(
-            leading: Icon(Icons.business_center),
-            title: Text(
-              'PROJECTS',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+          ButtonMenuItem(
+            title: 'PROJECTS',
+            icon: Icons.business_center,
+            onTap: () => goto(uiProvider.projectsKey),
           ),
-          ListTile(
-            leading: Icon(Icons.email_rounded),
-            title: Text(
-              'CONTACT',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-          )
+          ButtonMenuItem(
+            title: 'CONTACT',
+            icon: Icons.email_rounded,
+            onTap: () => goto(uiProvider.contactKey),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class ButtonMenuItem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const ButtonMenuItem({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      leading: Icon(icon),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleSmall,
       ),
     );
   }
