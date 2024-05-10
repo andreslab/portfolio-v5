3cf8e9cc6870473b82b1e0488850a094ae994315
Jaime Andrade <jaimeandrade454@gmail.com>
2024-05-10 10:13:49 -0500

    Remove path /api

--- lib/providers/constants.dart
+++ lib/providers/constants.dart
@@ -1,7 +1,7 @@
 class Environment {
   static const String _baseUrl = "https://api.andreslab.com";
-  static const String _apiPath = "/api";
+  // static const String _apiPath = "/api";
   static const String _assetsPath = "/assets";
   static const String assetsUrl = "$_baseUrl$_assetsPath";
-  static const String apiUrl = "$_baseUrl$_apiPath";
+  static const String apiUrl = _baseUrl;
 }
