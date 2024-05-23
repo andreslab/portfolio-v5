179984d2c7a7dc037895e9978219b082e33ce7e9
Jaime Andrade <jaimeandrade454@gmail.com>
2024-05-23 16:12:29 -0500

    Use Scaffold in full screen mode for projects screen

--- lib/widgets/projects/projects.dart
+++ lib/widgets/projects/projects.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:porfolio/providers/providers.dart';
 import 'package:porfolio/widgets/projects/widgets/body.dart';
+import 'package:provider/provider.dart';
 
 class Projects extends StatelessWidget {
+  final bool isfullScreen;
   const Projects({
     super.key,
+    this.isfullScreen = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Body();
+    final uiProvider = Provider.of<UiProvider>(context);
+    uiProvider.isSingleScreen = isfullScreen;
+    if (isfullScreen) {
+      return Scaffold(
+        body: Body(),
+      );
+    } else {
+      return Body();
+    }
   }
 }
